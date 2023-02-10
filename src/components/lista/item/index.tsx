e1c6229ef94f1cee3c71f9bb1112f7d14443ed16
Gabriel Fasolim <95487111+GabrielFasolim@users.noreply.github.com>
2023-02-10 18:32:11 -0300

    captando itens lista

--- src/components/lista/item/index.tsx
+++ src/components/lista/item/index.tsx
@@ -1,9 +1,31 @@
 import style from '../Lista.module.scss';
+import { ITarefa } from '../../../types/tarefa';
 
+interface Props extends ITarefa{
+  selecionaTarefa:(tarefaSelecionada: ITarefa) => void
+}
 
-export default function item({tarefa, tempo}:{tarefa:string, tempo:string}){
+export default function item(
+  {tarefa, 
+    tempo, 
+    selecionado, 
+    completado, 
+    id, 
+    selecionaTarefa}:Props){
+  console.log('Item atual: ')
   return(
-    <li className={style.item}>
+    <li
+     className={style.item}
+      onClick={() => selecionaTarefa(
+        {
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id
+        }
+    )}
+    >
         <h3>{tarefa}</h3>
         <span>{tempo}</span>
     </li>
